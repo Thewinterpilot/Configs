e6317f6a0e6777da55434326cf68caec6ed6a47f
thewinterpilot <soundofraindrops@proton.me>
2025-08-30 16:10:30 -0700

    changed update.bash and ran it

--- update.bash
+++ update.bash
@@ -1,14 +1,22 @@
-echo copying nixos files
+echo copying nixos config files...
 cp --update /etc/nixos/configuration.nix ~/Configs/nixos/configuration.nix
 cp --update /etc/nixos/home.nix ~/Configs/nixos/home.nix
 cp --update /etc/nixos/systempackages.nix ~/Configs/nixos/systempackages.nix
-echo copying config files
-
-cp -r --update .config/alacritty ~/Configs/.config/
-cp -r --update .config/folder ~/Configs/.config/
 
 
+echo copying .config files...
+cp --update ~/.config/hyfetch.json ~/Configs/.config/
 
-echo updated files
-echo please remember to manually copy new important configs to ~/Configs/
+
+echo copying .config folders...
+cp -r --update ~/.config/alacritty ~/Configs/.config/
+cp -r --update ~/.config/dunst ~/Configs/.config/
+cp -r --update ~/.config/gtk-3.0 ~/Configs/.config/
+cp -r --update ~/.config/gtk-4.0 ~/Configs/.config/
+cp -r --update ~/.config/hypr ~/Configs/.config/
+cp -r --update ~/.config/walker ~/Configs/.config/
+cp -r --update ~/.config/waybar ~/Configs/.config/
+
+echo please remember to manually copy new important configs to this script update.bash
+echo please update github through codium when you make signifigant changes
 echo have a good day winter:3
