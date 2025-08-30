d7d32fc1bc5a308405bb12e4b191d289f5891cd2
thewinterpilot <soundofraindrops@proton.me>
2025-08-29 18:39:59 -0700

    added home.nix and update.bash

--- nixos/home.nix
+++ nixos/home.nix
@@ -20,31 +20,25 @@
       shellAliases = {
         btw = "echo I use nixos btw";
         v = "nvim";
-        sv = "sudo nvim";
+        vh = "nvim .";
+	sv = "sudo nvim";
+	svh = "sudo nvim .";
         code = "codium";
-        c = "codium";
-        rs = "sudo nixos-rebuild switch";
+        rs = "sudo nixos-rebuild switch && bash ~/.config/update.bash";
         cff = "codium /etc/nixos/";
         vff = "sudo nvim /etc/nixos/";
 	hypr = "nvim ~/.config/hypr/";
-
+	c = "clear";
+	hy = "hyfetch";
+	chy = "clear && hyfetch";
     };
 
   #startup message 
     initExtra = ''
         export PS1='\[\e[38;5;207m\]\u\[\e[38;5;34m\]\H\[\e[0m\] \[\e[38;5;214m\]\w\[\e[0m\\$ '
       '';
-  }; ######THIS IS THE END OF THE PROGRAMS.BASH 
-
-  #alacritty config maybe
-    programs.alacritty.settings = {
-      window = {
-        opacity = 0.6;
-	};
-      };
+  }; ######THIS IS THE END OF THE PROGRAMS.BASH	SEGMENT
   
-  
-   
 
 
 
---
+++ update.bash
@@ -0,0 +1,8 @@
+echo copying nixos files
+cp --update /etc/nixos ~/Configs/
+echo copying config files
+cp -r --update .config Configs/.config
+
+echo updated files
+echo please remember to manually copy new important configs to ~/Configs/
+echo have a good day winter:3
