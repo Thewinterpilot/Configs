87d0237e5c9b187d9bb0e776b63cdca89f4ba37f
thewinterpilot <soundofraindrops@proton.me>
2025-08-30 15:51:17 -0700

    fixed save issue

--- update.bash
+++ update.bash
@@ -1,9 +1,12 @@
 echo copying nixos files
-cp --update /etc/nixos ~/Configs/
+cp --update /etc/nixos/configuration.nix ~/Configs/configuration.nix
+cp --update /etc/nixos/home.nix ~/Configs/home.nix
+cp --update /etc/nixos/systempackages.nix ~/Configs/systempackages.nix
 echo copying config files
-cp -r --update .config ~/Configs/.config
-rm -f ~/Configs/nixos/hardware-configuration.nix
-rm -rf ~/Configs/.config/.config
+
+cp -r --update .config ~/Configs/
+
+
 echo updated files
 echo please remember to manually copy new important configs to ~/Configs/
 echo have a good day winter:3
