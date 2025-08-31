ea379fb59d1fd6950dc28bb82423a4618897d8ea
thewinterpilot <soundofraindrops@proton.me>
2025-08-31 13:34:40 -0700

    changed waybar configs

--- nixos/systempackages.nix
+++ nixos/systempackages.nix
@@ -18,93 +18,89 @@
     services.auto-cpufreq.enable = true;  #changes frequency based on things idk
 
 
-  ##systempackages
+  ## systempackages
     environment.systemPackages = with pkgs; [
-    ##visible apps (not terminal tools or dependancies)
+    ## visible apps (not terminal tools or dependancies)
 
       #Text editors
         neovim              #terminal text editor
         vscodium            #gui IDE
       #terminals
-        foot		    #terminal emulator
-	alacritty
+	      alacritty           #allows what I need with easy syntax
       #browsers
-        librewolf           #browser
+        librewolf           #private secure browser w customisation
       #login manager
         ly
       #hyprland specific things
         hyprland            #window manager
         waybar              #bar
-	walker              #app launcher
+	      walker              #app launcher
         hyprpaper           #wallpaper utility
         hyprshot            #screenshot utility
-	hyprlock            #lock screen utility
-	  libfprint
+	      hyprlock            #lock screen utility
       #git
         git                 #idk its git, self explanitory
       #file explorers
-        nnn		    #terminal file explorer
         kdePackages.dolphin #gui file explorer
-	yazi                #terminal file explorer 2
-	nautilus            #gui file explorer 2
+	      nautilus            #gui file explorer 2
+        yazi                #terminal file explorer
       #spec fetching utils
-        hyfetch		    #shows basic pc spec overlay
-        cpufetch	    #shows cpu spec overlay
-        btop		    #shows cpu/gpu/ram usage and list of running applications
+        hyfetch		          #shows basic pc spec overlay
+        cpufetch	          #shows cpu spec overlay
+        btop		            #shows cpu/gpu/ram usage and list of running applications
       #discord
-        vesktop 	    #foss discord with vencord
+        vesktop 	          #foss discord with vencord
       #windows app software
-        bottles		    #run windows apps in "bottles"
-	virtualbox          #virtual machine software
+        bottles		          #run windows apps in "bottles"
+	      virtualbox          #virtual machine software
       #video tools
-        vlc		    #video player ive always used idk
+        vlc		              #video player ive always used idk
         shotcut             #video editor
-	libsForQt5.kdenlive #video editor 2
+	      libsForQt5.kdenlive #video editor 2
       #theme changer
         themechanger        #theme changer
       #qbittorrent
         qbittorrent         #torrent things
       #note apps
-	obsidian            #notes
+	      obsidian            #markdown notes locally
       #mc
-        prismlauncher       #mc idk
+        prismlauncher       #customisable minecraft client with easy mod support 
       #music player
-        aonsoku             #server gui mp
-	musikcube           #local tui mp
-	museeks             #local gui mp
+	      museeks             #local gui music player
       #game engine
         godot               #simple 2d/3d game engine
-    ##dependencies
-      #assorted
-        lxqt.lxqt-policykit #allows permissions for vscode sudo
-        brightnessctl       #allows brightness keys to work
-      #cloud things
-        #onedrive
-	  onedriver
-        #samba shares
+       #cloud things
+	      onedriver           #connect to onedrive
+	    #warpinator
+	      warpinator          #warp files across lan
+      #screenshot
+	      grimblast           #screenshot manager
+      #tldr
+        tldr                #shows tldr of a commands manpage
+      #bluetooth
+        blueberry           #gui bluetooth tool
+
+    ## dependencies
+      #vscodium
+      lxqt.lxqt-policykit   #allows permissions for vscode sudo
+      #brightness
+      brightnessctl         #allows brightness keys to work
+      #samba shares
           cifs-utils        #network shares terminal util
-          samba		    #allows connecting to network shares
+          samba		          #allows connecting to network shares
       #python3
         python3             #python
       #fastfetch
         fastfetch           #needed for hyfetch
       #notifications
         dunst               #notification deamon
-	libnotify           #apperently important for dunst
+	      libnotify           #apperently important for dunst
       #killall command
         killall             #used for the toggling of waybar
-      #screenshot
-	grimblast           #screenshot manager
-      #tldr
-        tldr                #tldr, gives tldr of a commands manpages
       #ark
         kdePackages.ark     #zip and unzip files
-      #bluetooth
-        blueberry           #bluetooth tool
-	bluez
-	bluez-tools
-      #git dependency
-        rPackages.askpass
+
+
     ];
 
 }
