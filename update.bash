b1a1d8c651be4627f1b78d01d649b48815bf9b18
thewinterpilot <soundofraindrops@proton.me>
2025-09-01 19:33:45 -0700

    changed update.bash

--- update.bash
+++ update.bash
@@ -18,10 +18,11 @@ cp -r --update ~/.config/walker ~/Configs/.config/
 cp -r --update ~/.config/waybar ~/Configs/.config/
 
 echo pushing to git...
+cd ~/Configs
 git add -A
 git commit -a
 git push
-
+cd 
 echo please remember to manually copy new important configs to this script update.bash
 echo please update github through codium when you make signifigant changes
 echo have a good day winter:3
