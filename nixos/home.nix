f653eeda123c34adb299cfe764e8f477b7da44df
thewinterpilot <soundofraindrops@proton.me>
2025-09-09 10:17:24 -0700

    added some comments to things and added nitch

--- nixos/home.nix
+++ nixos/home.nix
@@ -15,10 +15,11 @@
       enable = true;
       shellAliases = {
         
-        vc = "vim";
+        v = "vim";
 	      sv = "sudo vim";
         code = "codium";
         rs = "sudo nixos-rebuild switch && bash ~/Configs/update.bash";
+	      up = "bash ~/Configs/update.bash";
         nx = "codium /etc/nixos/";
         ss = "nh search ";
         upgrade = "sudo nixos-rebuild switch --upgrade";
@@ -32,9 +33,6 @@
         export PS1='\[\e[38;5;207m\]\u\[\e[38;5;34m\]\H\[\e[0m\] \[\e[38;5;214m\]\w\[\e[0m\\$ '
       '';
   }; ######THIS IS THE END OF THE PROGRAMS.BASH	SEGMENT
-  
-
-
 
 }
 
