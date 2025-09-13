1254df691c97e0658ec84b8d66264e47005c2d0a
thewinterpilot <soundofraindrops@proton.me>
2025-09-13 10:29:39 -0700

    added waypaper

--- nixos/userpkgs.nix
+++ nixos/userpkgs.nix
@@ -43,16 +43,9 @@ home-manager
           ##hyprland specific things
             waybar          #decent bar
 	          walker          #app launcher
-            hyprpaper       #wallpaper utility
+            waypaper       #wallpaper utility
 	          hyprlock        #lock screen utility
-            hyprpanel       #panel for hyprland
-            #hyprpanel dependencies
-              pywal
-              wf-recorder
-              hyprsunset
-              matugen
-	      swww
-              upower
+	      swww          #wallpaper setter
           ##other cli tools
             tree            #show file trees
             nh              #nixos search utility
